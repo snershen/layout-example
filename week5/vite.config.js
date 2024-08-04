da0bc44e57d93671295458477b5273a21871f94a
Wu Jane <jane72418@gmail.com>
2024-08-04 22:22:15 +0800

    update week5 config

--- week5/vite.config.js
+++ week5/vite.config.js
@@ -25,7 +25,7 @@ function moveOutputPlugin() {
 export default defineConfig({
   // base 的寫法：
   // base: '/Repository 的名稱/'
-  base: '/2024-web-layout-training-code/week5/dist/',
+  base: '/layout-example/week5/dist/',
   plugins: [
     liveReload(['./layout/**/*.ejs', './pages/**/*.ejs', './pages/**/*.html']),
     ViteEjsPlugin(),
